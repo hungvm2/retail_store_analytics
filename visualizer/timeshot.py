1d96903c32a28f04ddb31e8cec52bdac3a39042e
hungvm2 <lolo.aquarius@gmail.com>
2022-05-26 22:19:48 +0700

    Completed code

--- visualizer/timeshot.py
+++ visualizer/timeshot.py
@@ -8,27 +8,17 @@ from dot import *
 class timeShots:
     def __init__(self, dat):
         self.data = dat
-
-        maxX = 0
-        maxY = 0
-        for i in range(0, len(self.data)):
-            hmdict = json.loads(self.data[i][-1])
-            for xy, z in hmdict.items():
-                d = dot(xy, z)
-                maxX = max(maxX, d.getX())
-                maxY = max(maxY, d.getY())
-        self.maxX = maxX
-        self.maxY = maxY
+        self.maxX = 20
+        self.maxY = 20
 
     def sumData(self):
-        dotMatrix = np.zeros((self.maxX + 1, self.maxY + 1))
-        # dotMatrix = np.zeros((200, 200))
+        dotMatrix = np.zeros((self.maxY + 1, self.maxX + 1))
         for i in range(0, len(self.data)):
             aShot = self.data[i]
             dotsData = json.loads(aShot[-1])  # lay cot "data"
             for xy, z in dotsData.items():  # phan giai toa do x, y, z value
                 d = dot(xy, z)
-                dotMatrix[d.getX(), d.getY()] += z
+                dotMatrix[d.getY(), d.getX()] += z
         return dotMatrix
 
     def sumDataByTime(self):
--- visualizer/visualize.py
+++ visualizer/visualize.py
@@ -1,13 +1,7 @@
 import matplotlib.pyplot as plt
-# from contextlib import closing
+
 from heatmap_db import *
 from timeshot import *
-# from scipy.stats import gaussian_kde
-# import numpy as np
-# import json
-
-# START_TIME = "2022-03-22 8:00:00"
-# END_TIME = "2022-05-25 9:00:00"
 
 
 class Visualizer:
@@ -21,8 +15,6 @@ class Visualizer:
              f"   and aggr_time<='{end_time}'"
         dat = self.db.getData(s1)
 
-        # self.db.close_connection()
-
         # 1. plot heat map by space
         tshots = timeShots(dat)
         m = tshots.sumData()
@@ -30,8 +22,12 @@ class Visualizer:
         img = plt.imread("frame.png")
         fig, ax = plt.subplots(figsize=(19.2, 10.8), dpi=100)
         ax.imshow(img)
-        ax.set_title("Customer dense by Space")
-        im = ax.imshow(m, extent=[1920, 0, 1080, 0], alpha=0.7)  # im = ax.imshow(m, alpha=1)
+        ax.set_title("Customer's Interest by Space")
+        for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
+                     ax.get_xticklabels() + ax.get_yticklabels()):
+            item.set_fontsize(20)
+        im = ax.imshow(m, extent=[1920, 0, 1080, 0], alpha=0.7)
+
         # Create colorbar
         cbar = ax.figure.colorbar(im, ax=ax)
         cbar.ax.set_ylabel("customer density", rotation=-90, va="bottom")
@@ -42,7 +38,6 @@ class Visualizer:
              f"where aggr_time>='{start_time}' " \
              f"   and aggr_time<='{end_time}'"
         dat2 = self.db.getData(s2)
-        # self.db.close_connection()
 
         # 2. plot bar chart by date
         tshots2 = timeShots(dat2)
@@ -51,7 +46,7 @@ class Visualizer:
         x = []
         y = []
         for cTime, val in d.items():
-            if x == []:
+            if len(x) == 0:
                 x.append(cTime[0:13])
                 y.append(val)
             elif x[-1] == cTime[0:13]:
@@ -59,60 +54,13 @@ class Visualizer:
             else:
                 x.append(cTime[0:13])
                 y.append(val)
-        # print(x)
-        # print(y)
+
         fig2, ax2 = plt.subplots(figsize=(19.2, 10.8))
-        # plt.subplots_adjust(bottom=0.3, top=0.9)
         ax2.bar(x, y)
         ax2.set_ylabel('Customer dense')
-        ax2.set_title('Customer dense by Time')
+        ax2.set_title("Customer's interest by Time")
         ax2.set_xticklabels(x, rotation='vertical')
-        plt.rcParams.update({'font.size': 32})
+        for item in ([ax2.title, ax2.xaxis.label, ax2.yaxis.label] +
+                     ax2.get_xticklabels() + ax2.get_yticklabels()):
+            item.set_fontsize(20)
         plt.show()
-
-    # def show_heatmap2(self, start_time, end_time):
-    #     sql_command = f"SELECT * from coordinates_aggregation WHERE aggr_time >= '{start_time}' AND aggr_time <= '{end_time}'"
-    #     with closing(self.db.conn.cursor()) as cursor:
-    #         cursor.execute(sql_command)
-    #         fetched_data = cursor.fetchall()
-    #
-    #     H = 1080
-    #     W = 1920
-    #     grid_nums = (20, 20)
-    #     grid_size = (int(W / grid_nums[0]), int(H / grid_nums[1]))
-    #
-    #     coords_accumulate = dict()
-    #
-    #     max_count = 0
-    #     for record in fetched_data:
-    #         for cell_pos, value in json.loads(record[-1]).items():
-    #             if cell_pos not in coords_accumulate:
-    #                 coords_accumulate[cell_pos] = 0
-    #             coords_accumulate[cell_pos] += value
-    #             max_count = max(max_count, coords_accumulate[cell_pos])
-    #     data = list()
-    #     for key, value in coords_accumulate.items():
-    #         lat_long = key.split("-")
-    #         lat = int(lat_long[0]) * grid_size[0] + grid_size[0] // 2
-    #         long = int(lat_long[-1]) * grid_size[1] + grid_size[1] // 2
-    #         for i in range(10 * value):
-    #             data.append([lat, long])
-    #     data = np.array(data)
-    #     x = data[:, 0]
-    #     y = data[:, 1]
-    #     k = gaussian_kde(np.vstack([x, y]))
-    #     xi, yi = np.mgrid[x.min():x.max():100j, y.min():y.max():100j]
-    #     zi = k(np.vstack([xi.flatten(), yi.flatten()]))
-    #
-    #     img = plt.imread('frame.png')
-    #     fig, ax = plt.subplots(figsize=(19.2, 10.8), dpi=100)
-    #
-    #     ax.pcolormesh(xi, yi, zi.reshape(xi.shape), alpha=0.7)
-    #
-    #     ax.set_xlim(x.min(), x.max())
-    #     ax.set_ylim(y.min(), y.max())
-    #
-    #     im = ax.imshow(img, extent=[x.min(), x.max(), y.min(), y.max()], aspect="auto")
-    #     # Create colorbar
-    #     cbar = ax.figure.colorbar(im, ax=ax)
-    #     cbar.ax.set_ylabel("customer density", rotation=-90, va="bottom")
